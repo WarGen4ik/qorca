952a27528183a8aec715411e6bae16228d357812
WarGen4ik <illko73@mail.ru>
2018-05-08 06:25:02 +0300

    fixing

--- qorca/wsgi.py
+++ qorca/wsgi.py
@@ -7,12 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qorca.settings")
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
