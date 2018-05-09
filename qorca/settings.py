b270d87bad09b50a4d8e64a2d2c51a7da4d085ec
WarGen4ik <illko73@mail.ru>
2018-05-09 19:54:46 +0300

    mail send error

--- qorca/settings.py
+++ qorca/settings.py
@@ -136,7 +136,7 @@ MEDIA_URL = '/media/'
 
 SESSION_COOKIE_AGE = 3600
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'qorca.messager@gmail.com'
 EMAIL_HOST_PASSWORD = 'qqazwsxdec1'
