e7d1ef2ab0a97975f73693ed6dabe22beceb67e4
WarGen4ik <illko73@mail.ru>
2018-05-11 17:10:33 +0300

    refactored settings

--- qorca/settings/base.py
+++ qorca/settings/base.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 SECRET_KEY = '7ad+hin-9=$2b-6x@rb80o@w7!e1e5(n_4j&%mx7fi2h1#z8ff'
 # Quick-start development settings - unsuitable for production
