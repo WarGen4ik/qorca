eebc7e547bb9414c751655915a681cedb0f4e3fd
WarGen4ik <illko73@mail.ru>
2018-05-27 15:33:56 +0300

    fixes

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from auth_main.models import ContactMessage
 from core.models import TeamRelationToUser, Team, Invitations, Competition, CompetitionTeam, CompetitionUser, Distance, \
@@ -11,7 +12,9 @@ admin.site.register(Competition)
 admin.site.register(CompetitionUser)
 admin.site.register(CompetitionTeam)
 admin.site.register(Distance)
-admin.site.register(RelayRace)
-admin.site.register(RelayRaceTeam)
+# admin.site.register(RelayRace)
+# admin.site.register(RelayRaceTeam)
 admin.site.register(ContactMessage)
 admin.site.register(UserDistance)
+
+admin.site.unregister(Group)
