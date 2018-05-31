b678c7f36a7a5494b4209a815a50d8a516017cd9
WarGen4ik <illko73@mail.ru>
2018-05-31 23:57:54 +0300

    fixes

--- qorca/settings/prod.py
+++ qorca/settings/prod.py
@@ -1,13 +1,13 @@
 import os
 
-DEBUG = True
+DEBUG = False
 
 BASE_URL = 'http://www.qorca.ml'
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_PORT = '587'
-# EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-# EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
-# EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+EMAIL_USE_TLS = True
 
 
 AWS_USER = 'qorca-user'
